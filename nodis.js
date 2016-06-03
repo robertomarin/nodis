ae6ee4f6d9eb797cdabf8a83c4e45f36e265c527
Roberto Marin <robertomarin@gmail.com>
2016-06-03 03:56:39 -0300

    initial version

---
+++ nodis.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-h, --host [host]', 'Redis Host')
+  .option('-p, --port [port]', 'Redis Port')
+  .parse(process.argv);
+
+console.log('Running Nodis with:');
+if (program.host) console.log('### redisHost: %s', program.host);
+if (program.port) console.log('### redisPort: %s', program.port);
+
+var redis = require("redis");
+var client = redis.createClient({host: program.host, port: program.port || 6379});
+
+
+var elapsed_time = function(note) {
+    var precision = 0; // 3 decimal places
+    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+    console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
+    start = process.hrtime(); // reset the timer
+};
+
+var start = process.hrtime();
+elapsed_time('antes redis');
+var get = client.get('unkown-key-128321932', function(err, reply) {
+  console.log(reply);
+  elapsed_time('depois redis');
+});
